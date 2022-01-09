f04ce8f63af882a4eb3488059861b7320fb2b6f0
Emrecan Balgün <deathlygrave@gmail.com>
2022-01-09 22:22:20 +0300

    add scroll project

---
+++ Bootstrap Projects/Scroll Project/scroll.js
@@ -0,0 +1,11 @@
+const navbar = document.querySelector('#navbar')
+
+window.addEventListener("scroll", function () {
+  const scrollHeight = window.pageYOffset;
+  const navHeight = navbar.getBoundingClientRect().height;
+  if (scrollHeight > navHeight) {
+    navbar.classList.add("fixed-nav");
+  } else {
+    navbar.classList.remove("fixed-nav");
+  }
+});
