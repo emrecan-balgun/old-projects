925eb2ba6d0e7d0082e4bad8303d7870b8af3a54
Emrecan Balgün <deathlygrave@gmail.com>
2022-01-17 23:21:18 +0300

    add old todo app

---
+++ Redux Projects/old-todo-app/src/components/Form.js
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+
+import { useDispatch } from 'react-redux';
+import { addTodo } from '../redux/todos/todosSlice';
+
+function Form() {
+    const [title, setTitle] = useState('');
+
+    const dispatch = useDispatch();
+
+    const handleSubmit = (e) => {
+        if(!title) return;
+        e.preventDefault();
+
+        dispatch(addTodo({ title }));
+        setTitle('');
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input className="new-todo" placeholder="What needs to be done?" value={title} onChange={(e) => setTitle(e.target.value)} autoFocus/>
+        </form>
+    )
+}
+
+export default Form
